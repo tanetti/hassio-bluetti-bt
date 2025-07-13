d011684cd56e2d6256e139dd3a2cf4bc64abe0bc
Volodymyr Dudka <tanetti22@gmail.com>
2025-07-13 17:35:29 +0300

    Switch to v1

--- custom_components/bluetti_bt/bluetti_bt_lib/base_devices/ProtocolV1Device.py
+++ custom_components/bluetti_bt/bluetti_bt_lib/base_devices/ProtocolV1Device.py
@@ -12,10 +12,10 @@ class ProtocolV1Device(BluettiDevice):
         self.struct = DeviceStruct()
 
         # Device info
-        self.struct.add_string_field("device_type", 10, 6)
+        self.struct.add_swap_string_field("device_type", 10, 6)
         self.struct.add_sn_field("serial_number", 17)
-        self.struct.add_version_field('arm_version', 23)
-        self.struct.add_version_field('dsp_version', 25)
+        # self.struct.add_version_field('arm_version', 23)
+        # self.struct.add_version_field('dsp_version', 25)
 
         # Power IO
         self.struct.add_uint_field("dc_input_power", 36)
@@ -24,19 +24,17 @@ class ProtocolV1Device(BluettiDevice):
         self.struct.add_uint_field("dc_output_power", 39)
 
         # Power IO statistics
-        self.struct.add_decimal_field(
-            "power_generation", 41, 1
-        )  # Total power generated since last reset (kwh)
+        # self.struct.add_decimal_field("power_generation", 41, 1)  # Total power generated since last reset (kwh)
 
         # Battery
         self.struct.add_uint_field("total_battery_percent", 43)
 
         # Output state
-        self.struct.add_bool_field("ac_output_on", 48)
-        self.struct.add_bool_field("dc_output_on", 49)
+        # self.struct.add_bool_field("ac_output_on", 48)
+        # self.struct.add_bool_field("dc_output_on", 49)
 
         # Pack selector
-        self.struct.add_uint_field("pack_num", 3006)  # internal
+        # self.struct.add_uint_field("pack_num", 3006)  # internal
 
         # Output controls
         self.struct.add_bool_field("ac_output_on_switch", 3007)
@@ -47,20 +45,20 @@ class ProtocolV1Device(BluettiDevice):
     @property
     def polling_commands(self) -> List[ReadHoldingRegisters]:
         return [
-            ReadHoldingRegisters(10, 10),
+            ReadHoldingRegisters(10, 20),
             ReadHoldingRegisters(36, 4),
-            ReadHoldingRegisters(41, 1),
+            # ReadHoldingRegisters(41, 1),
             ReadHoldingRegisters(43, 1),
-            ReadHoldingRegisters(48, 2),
+            # ReadHoldingRegisters(48, 2),
             ReadHoldingRegisters(3007, 2),
         ]
 
     @property
     def writable_ranges(self) -> List[range]:
-        return [range(3006, 3009)]
+        return [range(3007, 3009)]
     
-    @property
-    def pack_num_field(self) -> List[ReadHoldingRegisters]:
-        return [
-            ReadHoldingRegisters(96, 1),
-        ]
+    # @property
+    # def pack_num_field(self) -> List[ReadHoldingRegisters]:
+    #     return [
+    #         ReadHoldingRegisters(96, 1),
+    #     ]
--- custom_components/bluetti_bt/bluetti_bt_lib/devices/ac200pl.py
+++ custom_components/bluetti_bt/bluetti_bt_lib/devices/ac200pl.py
@@ -5,36 +5,20 @@ from typing import List
 from ..field_enums import ChargingMode
 # from ..field_enums import ChargingMode, AutoSleepMode, UpsMode
 from ..utils.commands import ReadHoldingRegisters
-from ..base_devices.ProtocolV2Device import ProtocolV2Device
+from ..base_devices.ProtocolV1Device import ProtocolV1Device
 
-class AC200PL(ProtocolV2Device):
+class AC200PL(ProtocolV1Device):
     def __init__(self, address: str, sn: str):
         super().__init__(address, "AC200PL", sn)
 
         # Power IO
-        # self.struct.add_sn_field('serial_number', 17)
-        # self.struct.add_version_field('arm_version', 23)
-        # self.struct.add_version_field('dsp_version', 25)
-        self.struct.add_uint_field('dc_input_power', 36)
-        self.struct.add_uint_field('ac_input_power', 37)
-        self.struct.add_uint_field('ac_output_power', 38)
-        self.struct.add_uint_field('dc_output_power', 39)
-        # self.struct.add_decimal_field('power_generation', 41, 1)  # Total power generated since last reset (kwh)
-        self.struct.add_uint_field('total_battery_percent', 43)
-        # self.struct.add_bool_field('ac_output_on', 48)
-        # self.struct.add_bool_field('dc_output_on', 49)
-        self.struct.add_decimal_field('ac_input_voltage', 77, 1)
+        # self.struct.add_decimal_field('ac_input_voltage', 77, 1)
         # self.struct.add_decimal_field('ac_input_frequency', 80, 2)
 
         # Controls (3000)
         # self.struct.add_enum_field('ups_mode', 3001, UpsMode)
-        # self.struct.add_bool_field('split_phase_on', 3004)
-        # self.struct.add_enum_field('split_phase_machine_mode', 3005, MachineAddress)
-        # self.struct.add_uint_field('pack_num', 3006)
         # self.struct.add_bool_field('ac_output_on', 3007)
         # self.struct.add_bool_field('dc_output_on', 3008)
-        self.struct.add_bool_field('ac_output_on_switch', 3007)
-        self.struct.add_bool_field('dc_output_on_switch', 3008)
         # self.struct.add_bool_field('grid_charge_on', 3011)
         # self.struct.add_bool_field('time_control_on', 3013)
         # self.struct.add_uint_field('battery_range_start', 3015)
@@ -45,18 +29,17 @@ class AC200PL(ProtocolV2Device):
         # 3039-3056 is the time control programming
         self.struct.add_bool_field('power_off', 3060)
         # self.struct.add_enum_field('auto_sleep_mode', 3061, AutoSleepMode)
-        self.struct.add_bool_field('silent_charging_on', 3065)
+        # self.struct.add_bool_field('silent_charging_on', 3065)
         self.struct.add_enum_field('charging_mode', 3065, ChargingMode)
         self.struct.add_bool_field('power_lifting_on', 3066)
 
     @property
     def polling_commands(self) -> List[ReadHoldingRegisters]:
         return super().polling_commands + [
-            ReadHoldingRegisters(10, 40),
-            ReadHoldingRegisters(70, 21),
-            ReadHoldingRegisters(3001, 66),
+            ReadHoldingRegisters(3060, 1),
+            ReadHoldingRegisters(3065, 2),
         ]
 
     @property
     def writable_ranges(self) -> List[range]:
-        return super().writable_ranges + [range(3000, 3067)]
+        return super().writable_ranges + [range(3060, 3067)]
